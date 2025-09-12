14acf58cd33e2356ee77d7509484d433c8a2a126
Amirhossein Asgharnia <asgharnia4@yahoo.com>
2025-09-11 21:07:13 -0400

    transform completed!

--- Document Scanner/transform.py
+++ Document Scanner/transform.py
@@ -1,2 +1,15 @@
 import numpy as np
 import cv2
+
+def order_points (pts):
+    rectangle = np.zeros ((4 , 2) , dtype="float32")
+
+    sum_pts = np.sum (pts , axis = 1)
+
+    rectangle[0] = pts (np.argmin(sum_pts))
+    rectangle[2] = pts (np.argmax(sum_pts))
+
+    diff_pts = np.sum (pts , axis = 1)
+
+    rectangle[1] = pts (np.argmin(sum_pts))
+    rectangle[3] = pts (np.argmax(sum_pts))
