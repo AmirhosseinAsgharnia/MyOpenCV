c6e61b2f88d4debb6ebb12ded7c5cdd65fbcbd08
Amirhossein Asgharnia <asgharnia4@yahoo.com>
2025-09-12 23:28:29 -0400

    Matrix updated

--- Document Scanner/transform.py
+++ Document Scanner/transform.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-def order_points (pts: np.array) -> np.array:
+def order_points(pts):
 
     # The function takes in four points, the corners, determine which corner is each point and returns it.
 
@@ -21,4 +21,17 @@ def order_points (pts: np.array) -> np.array:
 
 def four_point_transform (image, pts):
 
-    rectangle = 
+    rectangle = order_points(pts)
+    (tl , tr , br , bl) = rectangle
+
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    destination = np.array([[0,0],[maxWidth - 1, 0],[maxWidth - 1, maxHeight - 1],[0, maxHeight - 1]], dtype = "float32")
+    M = cv2.getPerspectiveTransform(rectangle, destination)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    return warped
